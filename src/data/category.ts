d6a190433f6f2948a777f7ccb6b4ba041bd973f3
niratamang <neera678@gmail.com>
2024-07-27 22:41:35 +0545

    feat: create category data

---
+++ src/data/category.ts
@@ -0,0 +1,32 @@
+export const CATEGORY = [
+  {
+    id: '1',
+    title: 'Seafood',
+    imageUrl: 'https://fabrx.co/preview/tastebite/assets/images/menus/menu91.png'
+  },
+  {
+    id: '2',
+    title: 'Soup',
+    imageUrl: 'https://fabrx.co/preview/tastebite/assets/images/menus/menu92.png'
+  },
+  {
+    id: '3',
+    title: 'Pizza',
+    imageUrl: 'https://fabrx.co/preview/tastebite/assets/images/menus/menu98.png'
+  },
+  {
+    id: '4',
+    title: 'Burger',
+    imageUrl: 'https://fabrx.co/preview/tastebite/assets/images/menus/menu101.png'
+  },
+  {
+    id: '5',
+    title: 'Ramen',
+    imageUrl: 'https://fabrx.co/preview/tastebite/assets/images/menus/menu109.png'
+  },
+  {
+    id: '6',
+    title: 'Dips',
+    imageUrl: 'https://fabrx.co/preview/tastebite/assets/images/menus/menu110.png'
+  }
+];
